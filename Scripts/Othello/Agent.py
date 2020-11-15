559ff7c594f01f22fd0e5e8096d86a1d9f8ad2de
AlexisF <alexisfabre.dev@gmail.com>
2020-11-16 00:00:35 +0100

    Better agent

--- Scripts/Othello/Agent.py
+++ Scripts/Othello/Agent.py
@@ -7,6 +7,8 @@ import numpy as np
 import tqdm
 import random
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 class Model(nn.Module):
 
@@ -15,25 +17,36 @@ class Model(nn.Module):
         # Input (2, 8, 8)
         self.input_layer = nn.Conv2d(2, 128, 3, padding=1)
         self.hidden_layer_1 = nn.Conv2d(128, 128, 3, padding=1)
-        self.hidden_layer_2 = nn.Conv2d(128, 128, 3, padding=1)
-        self.hidden_layer_3 = nn.Conv2d(128, 128, 3, padding=1)
-        self.hidden_layer_4 = nn.Conv2d(128, 128, 3, padding=1)
-        self.output_layer = nn.Conv2d(128, 1, 3, padding=1)
-        self.skip_turn = nn.Linear(128*8*8, 1)
+        self.hidden_layer_21 = nn.Conv2d(128, 128, 3, padding=1)
+        self.hidden_layer_22 = nn.Conv2d(128, 128, 3, padding=1)
+        self.hidden_layer_23 = nn.Conv2d(128, 128, 3, padding=1)
+        self.hidden_layer_3 = nn.Linear(128*8*8, 128*8)
+        self.hidden_layer_4 = nn.Linear(128*8, 128)
+        self.output_layer = nn.Linear(128, 65)
 
     def forward(self, x):
         x = F.relu(self.input_layer(x))
         x = F.relu(self.hidden_layer_1(x))
-        x = F.relu(self.hidden_layer_2(x))
+        x = F.relu(self.hidden_layer_21(x))
+        x = F.relu(self.hidden_layer_22(x))
+        x = F.relu(self.hidden_layer_23(x))
+        x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.hidden_layer_3(x))
         x = F.relu(self.hidden_layer_4(x))
-        output = F.softmax(self.output_layer(x))
-        skip = F.sigmoid(self.skip_turn(torch.flatten(x)))
-        return skip, output
+        output = self.output_layer(x)
+        return output
+    
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
 
 class Memory(object):
-    def __init__(self, max_memory=1000):
+    # def __init__(self, max_memory=100):
+    def __init__(self, max_memory=512):
         self.max_memory = max_memory  # maximum elements stored
         self.memory = list()  # initialize the memory
 
@@ -52,17 +65,17 @@ class Memory(object):
 
 
 class AgentTorch():
-    def __init__(self, epsilon=0.1, discount=0.99, batch_size=50):
+    def __init__(self, epsilon=0.3, discount=0.99, batch_size=50):
         self.epsilon = epsilon
         self.memory = Memory()
         # Discount for Q learning (gamma)
         self.discount = discount
         self.batch_size = batch_size
-        # self.model = Model(criterion=nn.MSELoss())
         self.model = Model()
-        # self.criterion = torch.nn.CrossEntropyLoss()
+        self.model.to(device)
         self.criterion = torch.nn.MSELoss()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.03)
+        # self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.3)
 
     def set_epsilon(self, e):
         self.epsilon = e
@@ -72,6 +85,7 @@ class AgentTorch():
         an integer between 0 and 4 (not included) with a random exploration of epsilon"""
         if train:
             if np.random.rand() <= self.epsilon:
+                # self.epsilon *= 0.9999
                 action = tuple(torch.randint(0, 8, (2,)).tolist())
             else:
                 action = self.learned_act(s)
@@ -83,18 +97,16 @@ class AgentTorch():
     def learned_act(self, s):
         """ Act via the policy of the agent, from a given observation s
         it proposes an action a"""
-        skip, output = self.model(s)
-        # print("############################################")
-        # print(skip)
-        if skip > 0.5: # Skip turn
+        output = self.model(s.to(device))
+        m = output.view(-1).argmax()
+        # print("m:")
+        # print(m.to("cpu").item())
+        if m == 64:
             return None
         else: # Play action
-            m = output.view(-1).argmax()
             i = (m % 8).view(-1, 1).item()
             j = (m // 8).view(-1, 1).item()
-            # print(output)
-            # print("i: ", i)
-            # print("j: ", j)
+            # print(i, j)
             return (i, j)
 
     def reinforce(self, s, n_s, a, r, game_over_):
@@ -111,30 +123,41 @@ class AgentTorch():
     
         # 2) Learn from the pool
         input_observations = torch.zeros((self.batch_size, 2, 8, 8))
-        target_q = torch.zeros((self.batch_size, 8, 8))
+        target_q = torch.zeros((self.batch_size, 65))
 
         if self.memory.size() < self.batch_size:  # if not enough elements in memory we do nothing
             return 1e5  # unknown (loss)
 
         samples = self.memory.random_access(self.batch_size)
 
-        for i in range(self.batch_size):
-            holder, next_s, a, r, end = samples[i]  # observation, next_observation, action, reward, game_over
-            input_observations[i] = torch.tensor(holder)
+        for i_batch in range(self.batch_size):
+            s, next_s, a, r, end = samples[i_batch]  # observation, next_observation, action, reward, game_over
+            input_observations[i_batch] = torch.tensor(s)
 
             # update the target
+            action_id = 0
+            if a == None:
+                action_id = 64
+            else:
+                (i, j) = a
+                action_id = i + 8 * j
+
+                
             if end:
-                target_q[i, a] = r
+                target_q[i_batch, action_id] = r
             else:
                 # compute max_a Q(nex_observation, a) using the model
-                (pred_skip, pred_action) = self.model(torch.tensor(next_s, dtype=torch.float).unsqueeze(0))
-                Q_next_observation = torch.max(pred_action)
-
+                y_pred = self.model(torch.tensor(next_s, dtype=torch.float).unsqueeze(0).to(device)).to("cpu")
+                Q_next_observation = torch.max(y_pred)
+                
                 # r + gamma * max_a Q(nex_observation, a)
-                target_q[i, a] = r + self.discount * Q_next_observation
+                target_q[i_batch, action_id] = r + self.discount * Q_next_observation
+                # if action_id in [20, 29, 34, 43]:
+            # print(action_id, target_q[i_batch, action_id].item())
 
         # HINT: Clip the target to avoid exploding gradients.. -- clipping is a bit tighter
         target_q = torch.clip(target_q, -3, 3)
+
 
         # train the model on the batch
         input_data = torch.tensor(input_observations) #[input_observations[i] for i in range(self.batch_size)])
@@ -144,8 +167,8 @@ class AgentTorch():
 
     def train_on_batch(self, x, y):
         self.model.train()
-        (skip_pre, y_pre) = self.model(torch.tensor(x, dtype=torch.float))
-        loss = self.criterion(torch.tensor(y, dtype=torch.float), y_pre)
+        y_pre = self.model(torch.tensor(x, dtype=torch.float).to(device))
+        loss = self.criterion(torch.tensor(y, dtype=torch.float).to(device), y_pre)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -164,9 +187,9 @@ class AgentTorch():
 def reward_from_signal(signal: Signal):
     reward = 0
     if signal is Signal.ILLEGAL_MOVE:
-            reward = -10000
+            reward = -1
     elif signal is Signal.VALID_MOVE:
-        reward = 1
+        reward = 5
     else: # Game over
         winner = board.get_winner()
         if winner == 1: # White
@@ -186,7 +209,8 @@ def train(agent: AgentTorch, board: Board, epoch):
     score = 0
     loss = 0
 
-    for e in tqdm.tqdm(range(epoch)):
+    # for e in tqdm.tqdm(range(epoch)):
+    for e in range(epoch):
         # At each epoch, we restart to a fresh game and get the initial observation
         observation, signal = board.reset()
         observation = torch.tensor(observation)
@@ -197,6 +221,7 @@ def train(agent: AgentTorch, board: Board, epoch):
         win = 0
         lose = 0
 
+        itour = 0
         while not game_over:
             # Keep old observation in memory
             prev_observation = observation
@@ -207,11 +232,13 @@ def train(agent: AgentTorch, board: Board, epoch):
             # Perform the action
             observation, signal = board.step(action)
             observation = torch.tensor(observation)
-            # print(f"Action: {action}: {signal}")
 
             # Reward attribution
-            reward = reward_from_signal(signal)
+            reward = reward_from_signal(signal) + itour
+
             game_over = game_over_from_signal(signal)
+            if signal is not Signal.ILLEGAL_MOVE:
+                print(f"Action: {action}: {signal}; {game_over}")
 
             # Update the counters
             if reward > 0:
@@ -219,22 +246,40 @@ def train(agent: AgentTorch, board: Board, epoch):
             if reward < 0:
                 lose = lose - reward
 
-            # Apply the reinforcement strategy
-            loss = agent.reinforce(prev_observation, observation, action, reward, game_over)
-
-            # Random BLACK agent
-            action = board.sample()
-            observation, signal = board.step(action)
-            game_over = game_over_from_signal(signal)
+            if game_over:
+                # Apply the reinforcement strategy
+                loss = agent.reinforce(prev_observation, observation, action, reward, game_over)
+                break
+            else:
+                # Random BLACK agent
+                black_action = board.sample()
+                observation, signal = board.step(black_action)
+                game_over = game_over_from_signal(signal)
+                loss = agent.reinforce(prev_observation, observation, action, reward, game_over)
+                itour += 1
 
         # Update stats
         score += win - lose
 
         print(f"Epoch {e}/{epoch}, loss {round(np.float64(loss), 4)}, win/lose count {win}/{lose} ({win - lose})")
+    
+    observation, signal = board.reset()
+    torch_observation = torch.tensor(observation, dtype=torch.float).unsqueeze(0).to(device)
+    output = agent.model(torch_observation)
+    print(output.to("cpu"))
+    action = agent.act(torch.tensor(observation, dtype=torch.float).unsqueeze(0))
+    observation, signal = board.step(action)
+
+    black_action = board.sample()
+    observation, signal = board.step(black_action)
+
+    torch_observation = torch.tensor(observation, dtype=torch.float).unsqueeze(0).to(device)
+    output = agent.model(torch_observation)
+    print(output.to("cpu"))
 
 
 if __name__ == '__main__':
-    epoch = 1000
+    epoch = 2048# 100000
     board = Board()
-    agent = AgentTorch(batch_size=1)
+    agent = AgentTorch(batch_size=32)
     train(agent, board, epoch)
--- Scripts/Othello/Board.py
+++ Scripts/Othello/Board.py
@@ -112,7 +112,8 @@ class Board():
         actions = self.get_possible_actions()
         if len(actions) == 0:
             return None
-        return random.choice(actions)
+        # return random.choice(actions)
+        return actions[0]
 
     def get_flips_in_dir(self, i, j, di, dj):
         # Move a step forward in the direction
